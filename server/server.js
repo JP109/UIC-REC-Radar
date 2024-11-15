94e7c3c1c8c26067529463f3f36f25bdb22d3bfa
JP109 <jaipawar113@gmail.com>
2024-11-15 15:45:40 -0600

    chore: Updated cors policy to allow all

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 // server.js
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Import Routes
